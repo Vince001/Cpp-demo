ce6ba5e438c7f2f2aafa07bba398598137ce587d
Vince <xcc001@qq.com>
2014-06-18 14:31:56 +0800

    Add some comments with Doxygen style.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,26 @@
 // haha, I can edit it here !!! reside github.com
 // modify in branch development
 
+/**
+ * @func Card_Error SamCard::Init(void)
+ * @brief Sam card init
+ *
+ * It will do the following process:
+ * - Init the Contact Card Reader
+ * - Confirm the psam,lsam card(Authenticate) exists.  @Ref AdpuCommands_1.0.xls Page: Demonstration
+ * - Execute AdpuCommands, eg:
+ *     -#SAM_GetAppCode
+ *     -#SAM_GetUid
+ *     -#...
+ *
+ *
+ * - Will call loadkey();
+ *
+ * @TODO: U8 terminalKey[24]={0};
+ * */
 int main( int argc, char **argv )
 {
     std::cout << "Hello,world." << std::endl;
+    //system( "pause" );
 }
+
