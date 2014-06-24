23fd92ae7a6c5b13ee2ef4f16523c7136fd580d9
Vince <xcc001@qq.com>
2014-06-24 09:35:01 +0800

    update work_record.ods file

--- main.cpp
+++ main.cpp
@@ -22,7 +22,12 @@
  * */
 int main( int argc, char **argv )
 {
+    int arr[8] = {5};
     std::cout << "Hello,world." << std::endl;
     //system( "pause" );
+    for(int i=0; i<8; i++)
+    {
+        std::cout << arr[i] << std::endl;
+    }
 }
 
